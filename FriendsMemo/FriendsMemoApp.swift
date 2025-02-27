57fd772a37bcb46e71b55075b54c4dec2c7a41b1
Gizem <gizemcsk1@gmail.com>
2025-02-27 12:26:01 +0100

    first commit

---
+++ FriendsMemo/FriendsMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsMemoApp.swift
+//  FriendsMemo
+//
+//  Created by Gizem Coskun on 27/02/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
