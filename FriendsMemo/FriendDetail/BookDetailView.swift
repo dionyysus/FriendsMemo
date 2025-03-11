2b2f0a37907bd715f3a6d6886aa3886fad63846d
Fatemehsharif2002 <fatemehsh0505@gmail.com>
2025-03-11 09:54:38 +0100

    test

---
+++ FriendsMemo/FriendDetail/BookDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  BookDetailView.swift
+//  FriendsMemo
+//
+//  Created by Fatemeh sharifmoghadam on 11/03/25.
+//
+
+import SwiftUI
+
+struct BookDetailViewOne: View {
+    var body: some View {
+        Text("hello")
+    }
+}
+
+#Preview {
+    BookDetailViewOne()
+}
